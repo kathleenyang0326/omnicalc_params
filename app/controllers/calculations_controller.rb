98526513dee0be861b1abf0c92736b98bcc27f48
Kathleen Yang <kathleen.yang@users.noreply.github.com>
2017-04-23 01:17:23 -0500

    flexible square completed

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,17 +1,28 @@
 class CalculationsController < ApplicationController
+def home_page
+
+
+
+
+end
+
   def flexible_square
     @the_number=params["zebra"].to_f
+    @output=@the_number**2
     render ("calculations/flexible_square.html.erb")
 end
 def flexible_square_root
   @the_number=params["zebra"].to_f
+  @output=@the_number**0.5
   render ("calculations/flexible_square_root.html.erb")
 end
 def flexible_payment
   @monthly_rate=params["a"].to_f/10000/12
   @periods=params["b"].to_f*12
   @present_value=params["c"].to_f
-  @the_number=
+  @screen_display_monthly_rate=params["a"].to_f/100
+  @screen_display_period=params["b"].to_f
+  @output=@monthly_rate*@present_value/(1-(1+@monthly_rate)**(-@periods))
   render ("calculations/flexible_payment.html.erb")
 end
 def square_form
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get("/",{:controller=>"calculations",:action=>"home_page"})
   get("flexible/square/:zebra",{:controller=>"calculations",:action=>"flexible_square"})
-  get("flexible/square/root/:zebra",{:controller=>"calculations",:action=>"flexible_square_root"})
+  get("flexible/square_root/:zebra",{:controller=>"calculations",:action=>"flexible_square_root"})
 get("flexible/payment/:a/:b/:c",{:controller=>"calculations",:action=>"flexible_payment"})
 get("/square/new",{:controller=>"calculations",:action=>"square_form"})
 end
