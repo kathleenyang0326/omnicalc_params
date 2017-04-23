8a226567e59ca224d795d8f3f9ae9553db050400
Kathleen Yang <kathleen.yang@users.noreply.github.com>
2017-04-23 18:38:53 -0500

    updated

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -70,5 +70,44 @@ def word_count_results
   @occurrences = @text.gsub(/[^a-z0-9\s]/i, "").downcase.split.count(@special_word.downcase)
 end
 def descriptive_statistics_results
+    @numbers = params[:list_of_numbers].gsub(',', '').split.map(&:to_f)
+    @sorted_numbers = @numbers.sort
+
+    @count = @numbers.count
+
+    @minimum = @numbers.sort.first
+
+    @maximum =  @numbers.sort.last
+
+    @range = @maximum-@minimum
+
+    if @count%2==0
+      number1=@count/2-1
+      number2=@count/2
+      @median=(@sorted_numbers[number1]+@sorted_numbers[number2])/2
+    end
+
+    if @count%2==1
+      number1=(@count+1)/2-1
+      @median=@sorted_numbers[number1]
+    end
+
+    @sum = @numbers.sum
+
+    @mean = @sum/@count
+
+    addingvariance=[]
+    @numbers.each do |num|
+      variancenumber=(num-@mean)**2
+      addingvariance.push(variancenumber)
+    end
+
+    @variance = (addingvariance.sum)/@count
+
+    @standard_deviation = @variance**0.5
+
+    freq = @numbers.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+
+    @mode = @numbers.max_by { |v| freq[v] }
 end
 end
