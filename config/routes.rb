0e51c642a8750017b1c646e6151d3cf475e60aea
Kathleen Yang <kathleen.yang@users.noreply.github.com>
2017-04-23 16:23:12 -0500

    updated part 1 and some of part 2

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ get("flexible/payment/:a/:b/:c",{:controller=>"calculations",:action=>"flexible_
 get("flexible/random/:c/:d",{:controller=>"calculations",:action=>"flexible_random_number"})
 get("/square/new",{:controller=>"calculations",:action=>"square_form"})
 get("/square/results",{:controller=>"calculations",:action=>"square_results"})
+get("/square_root/new",{:controller=>"calculations",:action=>"square_root_form"})
+get("/square_root/results",{:controller=>"calculations",:action=>"square_root_results"})
 end
